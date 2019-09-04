e4fed55975918c3ce064866d8cdf13421179259e
Jacob Burroughs <jburroughs@instructure.com>
2019-09-03 22:35:02 -0500

    Add scoping to admin pages

--- api/app/models/division.rb
+++ api/app/models/division.rb
@@ -1,4 +1,6 @@
 class Division < ApplicationRecord
+  scope :current_season, -> { joins(:league).where(leagues: { season: Season.active.first }) }
+
   belongs_to :league
   has_many :divisions_teams, dependent: :destroy
   has_many :teams, through: :divisions_teams
@@ -6,5 +8,11 @@ class Division < ApplicationRecord
 
   def team_count
     divisions_teams.size
+  end
+
+  rails_admin do
+    list do
+      scopes [:current_season, nil]
+    end
   end
 end
