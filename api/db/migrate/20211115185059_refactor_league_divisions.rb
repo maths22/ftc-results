d618c47542005dbf3fe6445a49827cdc64cd2f37
Jacob Burroughs <jburroughs@instructure.com>
2021-11-17 15:38:45 -0600

    Fix division migration

--- api/db/migrate/20211115185059_refactor_league_divisions.rb
+++ api/db/migrate/20211115185059_refactor_league_divisions.rb
@@ -9,7 +9,7 @@ class RefactorLeagueDivisions < ActiveRecord::Migration[6.1]
     add_reference :leagues, :league, foreign_key: true, index: true
 
     Division.all.each do |div|
-      l = League.create!(slug: div.slug, name: div.name, season: div.season, league: div.league)
+      l = League.create!(slug: div.slug, name: div.name, season: div.league.season, league: div.league)
 
       DivisionTeams.where(division: div).each do |dt|
         LeagueTeam.create!(league: l, team: dt.team)
